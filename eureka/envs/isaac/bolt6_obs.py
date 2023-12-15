cbf1aad7fa5229ab1a1b23562ff2a70b2aa151d0
rui_dyros21 <threeman1@snu.ac.kr>
2023-12-15 17:37:38 +0900

    bolt6

---
+++ eureka/envs/isaac/bolt6_obs.py
@@ -0,0 +1,21 @@
+class Bolt6(VecTask):
+    """Rest of the environment definition omitted."""
+    def compute_observations(self):
+        self.gym.refresh_dof_state_tensor(self.sim)  # done in step
+        self.gym.refresh_actor_root_state_tensor(self.sim)
+        self.gym.refresh_net_contact_force_tensor(self.sim)
+        self.gym.refresh_dof_force_tensor(self.sim)
+
+        self.obs_buf[:] = compute_anymal_observations(  # tensors
+                                                        self.root_states,
+                                                        self.commands,
+                                                        self.dof_pos,
+                                                        self.default_dof_pos,
+                                                        self.dof_vel,
+                                                        self.gravity_vec,
+                                                        self.actions,
+                                                        self.lin_vel_scale,
+                                                        self.ang_vel_scale,
+                                                        self.dof_pos_scale,
+                                                        self.dof_vel_scale
+        )
